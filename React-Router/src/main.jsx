a715db1330c23d287a10bdf2c58ca86d511f5a7a
Devansh Rajput <rajputdevansh012@gmail.com>
2024-01-19 21:25:05 +0530

    Add more functionalities React-Router

--- React-Router/src/main.jsx
+++ React-Router/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Layout from "./Layout";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     children: [
       { path: "", element: <Home /> },
       { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
     ],
   },
 ]);
